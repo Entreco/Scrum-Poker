16860cfefcd4b5bb8af7e51285199bd0fa6424ad
Remco Janssen <entreco@gmail.com>
2018-10-30 20:26:14 +0100

    # Add gradle setup

--- android/poker/src/main/java/nl/entreco/scrumpoker/poker/ui/poker/PokerFragment.kt
+++ android/poker/src/main/java/nl/entreco/scrumpoker/poker/ui/poker/PokerFragment.kt
@@ -26,7 +26,6 @@ class PokerFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PokerViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
